114062e20ffe4a85c8dd44ba8da6318c2d8c21af
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-12-29 15:33:32 +0700

    news details

---
+++ iot.vn/custom/js2.js
@@ -0,0 +1,89 @@
+﻿
+function subscribe() {
+    var emailpattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var email = $('#txtemail').val();
+    if (email != "") {
+        if (!emailpattern.test(email)) {
+            $('.text-danger').remove();
+            var str = '<span class="error">Invalid Email</span>';
+            $('#txtemail').after('<div class="text-danger">Invalid Email</div>');
+
+            return false;
+        }
+        else {
+            $.ajax({
+                url: 'index.php?route=extension/module/newsletters/news',
+                type: 'post',
+                data: 'email=' + $('#txtemail').val(),
+                dataType: 'json',
+
+
+                success: function (json) {
+
+                    $('.text-danger').remove();
+                    $('#txtemail').after('<div class="text-danger">' + json.message + '</div>');
+
+                }
+
+            });
+            return false;
+        }
+    }
+    else {
+        $('.text-danger').remove();
+        $('#txtemail').after('<div class="text-danger">Email Is Require</div>');
+        $(email).focus();
+
+        return false;
+    }
+
+
+}
+
+function quickbox() {
+    if ($(window).width() > 767) {
+        $('.quickview').magnificPopup({
+            type: 'iframe',
+            delegate: 'a',
+            preloader: true,
+            tLoading: 'Loading image #%curr%...'
+        });
+    }
+}
+
+
+
+$(document).ready(function () {
+
+    //$('#slideshow0').swiper({
+    //    mode: 'horizontal',
+    //    slidesPerView: 1,
+    //    pagination: '.slideshow0',
+    //    paginationClickable: true,
+    //    nextButton: '.swiper-button-next',
+    //    prevButton: '.swiper-button-prev',
+    //    spaceBetween: 0,
+    //    autoplay: 1500,
+    //    autoplayDisableOnInteraction: true,
+    //    loop: true,
+    //    speed: 1500
+    //});
+
+    quickbox();
+    jQuery(window).resize(function () { quickbox(); });
+
+    //$("#spinner").fadeOut("slow");
+
+    //$('.blogcarousel').owlCarousel({
+    //    items: 4,
+    //    singleItem: false,
+    //    navigation: true,
+    //    navigationText: ['<i class="fa fa-angle-left fa-5x"></i>', '<i class="fa fa-angle-right fa-5x"></i>'],
+    //    pagination: true,
+    //    itemsDesktop: [1000, 3],
+    //    itemsDesktopSmall: [979, 2],
+    //    itemsTablet: [767, 2]
+    //});
+
+    //$('#tabs a').tabs();
+});
